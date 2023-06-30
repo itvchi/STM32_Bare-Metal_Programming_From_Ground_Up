c65fc23c814336bfac3fc5671e220640b801337d
itvchi <jac.pikula@gmail.com>
2023-06-30 06:55:27 +0200

    ADC single conversion interrupt example

---
+++ 16_ADC_single_conversion_interrupt/Inc/adc.h
@@ -0,0 +1,11 @@
+#ifndef ADC_H_
+#define ADC_H_
+
+#include <stdint.h>
+
+void adc1_init();
+void adc1_interrupt_init();
+void adc_start_conversion();
+uint32_t adc_get_data();
+
+#endif /* ADC_H_ */
